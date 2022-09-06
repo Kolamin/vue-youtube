8a6322e889d1e2ac349f064830f1e0dee8765567
Anton Zhiliakov <gofranich@gmail.com>
2022-09-06 22:00:21 +0600

    Make views

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -1,12 +1,13 @@
 <template>
-  <div>
 
-  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'HomeView',
+  name: "Navbar"
 }
 </script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,59 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '@/views/Home.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: ()=> import('@/views/Home.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta:{layout:'empty'},
+    component: ()=> import('@/views/Login.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: ()=> import('@/views/Categories.vue')
+  },
+  {
+    path: '/detail',
+    name: 'detail',
+    meta: {layout: 'main'},
+    component: ()=> import('@/views/Detail.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: ()=> import('@/views/History.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: ()=> import('@/views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: ()=> import('@/views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: ()=> import('@/views/Record.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'main'},
+    component: ()=> import('@/views/Register.vue')
   }
 ]
 
